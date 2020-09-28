ef0b1f5fde5e89a852d89a18bb2ef6bd6c47974a
jrojas <jonathanrojas31@gmail.com>
2020-09-28 16:07:42 -0300

    adding full project heroes app

---
+++ src/selectors/getByheroesByPublisher.js
@@ -0,0 +1,12 @@
+const { heroes } = require("../data/heroes");
+
+export const getHeroesByPublisher = ( publisher ) => {
+
+    const validPublisher = ['DC Comics', 'Marvel Comics'];
+
+    if (!validPublisher.includes( publisher )) {
+        throw new Error(`Publisher "${ publisher }" no es correcto`)
+    }
+
+    return heroes.filter( hero => hero.publisher === publisher)
+}
